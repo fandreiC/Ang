297adb660687b5354cc62c5e8301b0fd8bcabffc
afratiman <andrei_fratiman@beenear.com>
2020-03-03 17:19:38 +0200

    t26 reached !

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,23 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { TestComponent } from './test/test.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { DepartmentComponent } from './department/department.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DepDetailsComponent } from './dep-details/dep-details.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent},
-  { path: 'about', component: AboutComponent},
-  { path: 'test', component: TestComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'test', component: TestComponent },
+  { path: 'empLst' , component: EmployeeListComponent },
+  { path: 'empDtls' , component: EmployeeDetailComponent },
+  { path: 'depLst', component: DepartmentComponent },
+  { path: 'dep/:id', component: DepDetailsComponent },
+  { path: "**", component: PageNotFoundComponent },
 ];
 
 @NgModule({
@@ -18,3 +28,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ EmployeeListComponent, DepartmentComponent, PageNotFoundComponent, DepDetailsComponent ]
+// export const routingComponents = []
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
@@ -15,6 +15,13 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule  } from '@angular/material/progress-spinner';
 import { TestComponent } from './test/test.component';
+// import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { EmployeeService } from './employee.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DepDetailsComponent } from './dep-details/dep-details.component';
+// import { DepartmentComponent } from './department/department.component';
+
 
 @NgModule({
   declarations: [
@@ -22,6 +29,12 @@ import { TestComponent } from './test/test.component';
     HomeComponent,
     AboutComponent,
     TestComponent,
+   // EmployeeListComponent,
+    EmployeeDetailComponent,
+   // DepartmentComponent,
+   routingComponents,
+   PageNotFoundComponent,
+   DepDetailsComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,7 +48,7 @@ import { TestComponent } from './test/test.component';
     MatProgressSpinnerModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
